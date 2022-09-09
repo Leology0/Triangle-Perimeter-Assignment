8f19136fb90ab1b7faca63dbc548c8992920c850
Leology0 <leology42@gmail.com>
2022-09-09 10:01:01 -0600

    cool

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 // Triangle Perimeter Assignment Start Code
+document.getElementById("calculateBtn").addEventListener("click", btnClicked)
+
+function btnClicked() {
+    let xAPoint = +document.getElementById("xA-point");
+    let yAPoint = +document.getElementById("xA-point");
+    let xBPoint = +document.getElementById("xB-point");
+    let yBPoint = +document.getElementById("xB-point");
+    let xCPoint = +document.getElementById("xC-point");
+    let yCPoint = +document.getElementById("xC-point");
+
+    document.getElementById('perimeter-result').innerHTML = dist()
+
+
+
+    function dist(xPoint, yPoint) {
+        let distance = 
+        
+    }
+}
