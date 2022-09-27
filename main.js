0cc4bf796a5ec93f774f24c61cbc988e164b2ed3
Leology0 <leology42@gmail.com>
2022-09-27 09:04:00 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
 // Triangle Perimeter Assignment Start Code
 document.getElementById("calculateBtn").addEventListener("click", btnClicked)
 
-// Distance
+
 function btnClicked() {
+    // Points
     let xAPoint = +document.getElementById("xA-point").value;
     let yAPoint = +document.getElementById("xA-point").value;
     let xBPoint = +document.getElementById("xB-point").value;
     let yBPoint = +document.getElementById("xB-point").value;
     let xCPoint = +document.getElementById("xC-point").value;
     let yCPoint = +document.getElementById("xC-point").value;
+    // Distance Var
+    let abSide = dist(xAPoint, yAPoint, xBPoint, yBPoint)
+    let acSide = dist(xAPoint, yAPoint, xCPoint, yCPoint)
+    let bcSide = dist(xBPoint, yBPoint, xCPoint, yCPoint)
 
     // Result
-    document.getElementById('resultAB').innerHTML = dist(xAPoint, yAPoint, xBPoint, yBPoint)
-    document.getElementById('resultAC').innerHTML = dist(xAPoint, yAPoint, xCPoint, yCPoint)
-    document.getElementById('resultBC').innerHTML = dist(xBPoint, yBPoint, xCPoint, yCPoint)
+    document.getElementById('resultAB').innerHTML = abSide
+    document.getElementById('resultAC').innerHTML = acSide
+    document.getElementById('resultBC').innerHTML = bcSide
     // Perimeter Calculation
-    document.getElementById('perimeter-result').innerHTML = dist(xAPoint, yAPoint, xBPoint, yBPoint) + dist(xAPoint, yAPoint, xCPoint, yCPoint) + dist(xBPoint, yBPoint, xCPoint, yCPoint)
+    document.getElementById('perimeter-result').innerHTML = abSide + acSide + bcSide
 }
 // Distance Function
 function dist(x1Point, y1Point, x2Point, y2Point) {
